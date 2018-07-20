c57bbd594c757b209948464898f838c17830e7de
Yu Hua Cheng <yuhua.nyc@gmail.com>
2018-07-20 18:44:58 -0400

    erased solutions

--- src/naive_bayes_classifier.py
+++ src/naive_bayes_classifier.py
@@ -14,8 +14,7 @@ class NaiveBayesClassifier:
         :param data_path: file path for the training data
         :return None
         """
-        data = pd.read_csv(data_path)
-        self.training_data = data
+        raise NotImplementedError()
 
     def train(self, class_label: str):
         """
@@ -27,16 +26,7 @@ class NaiveBayesClassifier:
         """
         Given a record with features, return the predicted class in boolean (True => positive)
         """
-        EPSILON = 0.01  # small constant to not let probability vanish because of missing observation
-        prediction = {}
-
-        for label in [0, 1]:
-            unnormalized_prob = self.likelihood_dict.get('prior').get(label)
-            for key, val in record.items():
-                unnormalized_prob *= self.likelihood_dict.get(key).get(label).get(val, EPSILON)
-            prediction[label] = unnormalized_prob
-
-        return max(prediction, key=lambda k: prediction[k]) == 1
+        raise NotImplementedError()
 
     def _construct_likelihood_dict(self, class_label: str):
         """
@@ -48,17 +38,4 @@ class NaiveBayesClassifier:
         :param class_label: class label's column name in self.training_data, assuming every other columns are features
         :return None
         """
-
-        # compute prior first
-        prior_positive = self.training_data[class_label].mean()
-        self.likelihood_dict['prior'] = {1: prior_positive, 0: 1 - prior_positive}
-
-        # for every feature x, compute P(x | y)
-        for feature in self.training_data.columns:
-            if feature != class_label:
-                feature_prob_dict = {}
-                for label in [0, 1]:
-                    feature_cnt = self.training_data.ix[self.training_data[class_label] == label][feature].value_counts()
-                    feature_prob = feature_cnt / feature_cnt.sum()
-                    feature_prob_dict[label] = feature_prob.to_dict()
-                self.likelihood_dict[feature] = feature_prob_dict
+        raise NotImplementedError()
